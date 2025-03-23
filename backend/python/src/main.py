b6a5c56974a37fcd2cc948a847a78d2479537ec8
ce20480 <aaronvinod25@gmail.com>
2025-03-23 10:48:54 +0000

    Akave upload integration, plus docker command for running akavelink docker container. Poetry updated to use command and add new modules

--- backend/python/src/main.py
+++ backend/python/src/main.py
@@ -1,19 +1,21 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from src.api.routes.storage import storage_router
+from .api.routes.storage import storage_router
 
 app = FastAPI()
 
+# Configure CORS
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:3000",
-        "http://localhost:8000",
-        "http://127.0.0.1:8000"
+        "http://localhost:3000",    # Express server
+        "http://localhost:8000",    # FastAPI direct access
+        "http://127.0.0.1:8000",    
+        "http://localhost:5173",    # Vite development server
     ],
     allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["*"]
+    allow_headers=["*"],
 )
 
 # Mount routes
